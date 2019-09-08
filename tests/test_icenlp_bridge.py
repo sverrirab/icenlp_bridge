682d8404ac2471e41972318843938c37f24d1ff9
Sverrir Á. Berg <sab@keilir.com>
2019-09-08 17:34:15 +0000

    Disable unittests when running on Travis
    Since the NLP server is not running on Travis this does ont work.
    Run tests locally please!

--- tests/test_icenlp_bridge.py
+++ tests/test_icenlp_bridge.py
@@ -3,7 +3,7 @@
 
 """Tests for `icenlp_bridge` package."""
 
-
+import os
 import unittest
 
 from icenlp_bridge import init, parse
@@ -19,10 +19,12 @@ class TestIcenlp_bridge(unittest.TestCase):
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
+    @unittest.skipIf(os.environ.get('TRAVIS'), 'true')
     def test_parse1(self):
         result = parse('Áframhaldandi úrkoma í dag')
         self.assertIn('Áframhaldandi', result)
 
+    @unittest.skipIf(os.environ.get('TRAVIS'), 'true')
     def test_parse2(self):
         result = parse('Ytra Lón á Langanesi.')
         self.assertIn('Langanesi', result)
